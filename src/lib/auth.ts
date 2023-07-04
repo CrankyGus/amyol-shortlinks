3d77e3ad81cf72aa6dcc088564dbbe8308f0d0bf
จิรทีปต์ ลอยมา <jirateep.loyma02@gmail.com>
2023-07-05 05:04:35 +0700

    add - libs & auth opts

---
+++ src/lib/auth.ts
@@ -0,0 +1,28 @@
+import type { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "./prisma";
+import { Adapter } from "next-auth/adapters";
+
+export const authOpts: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  pages: {
+    signIn: "/signin",
+  },
+};
