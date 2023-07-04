a5612253974e7b90aac5bb263734bd6c516c0449
จิรทีปต์ ลอยมา <jirateep.loyma02@gmail.com>
2023-07-05 05:03:35 +0700

    add - types

---
+++ types/env.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    DISCORD_CLIENT_ID: string;
+    DISCORD_CLIENT_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    GITHUB_ID: string;
+    GITHUB_SECRET: string;
+    NEXT_PUBLIC_SITE_URL: string;
+  }
+}
