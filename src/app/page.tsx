572cf2c55f56e7e5eaa654a75b4e5873f6cf0d45
จิรทีปต์ ลอยมา <jirateep.loyma02@gmail.com>
2023-07-05 05:11:49 +0700

    prettier format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { User } from "@/components/user";
 import Input from "@/components/Input";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
@@ -8,19 +7,13 @@ export default async function Home() {
   const cookieStore = cookies();
   const session = cookieStore.get("next-auth.session-token");
   if (!session) return redirect("/signin");
+
   return (
     <div className="mt-24 flex max-w-[1200px] flex-col items-center justify-center">
       <h1 className="mb-8 font-bold md:text-3xl">
         Paste the <span className="text-red-500">URL</span> to be shortened
       </h1>
       <div className="">
-        {/* <LoginButton />
-        <RegisterButton />
-        <LogoutButton />
-        <ProfileButton /> */}
-
-        {/* <User /> */}
-
         <Input session={session?.value || ""} />
         <Datatable />
       </div>
